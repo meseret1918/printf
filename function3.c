aca2ef42b273c3c86c0918daa0e0099d674a07a4
meseret1918 <meseret1918@gmail.com>
2023-01-23 21:02:57 -0800

    printf

--- function3.c
+++ function3.c
@@ -141,7 +141,7 @@ int print_reverse(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
-int print_rot13(va_list types, char buffer[],
+int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char x;
