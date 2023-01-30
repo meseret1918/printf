0810b90623e394cdce8522e88a9bb122b9933b47
meseret1918 <meseret1918@gmail.com>
2023-01-30 07:24:17 -0800

    update printf

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PRINTF
+#define PRINTF
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
---
+++ precision.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * handle_precision - Matches a precision modifier with
+ *                    its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential precision modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a precision modifier is matched - its value.
+ *         If the precision modifier is empty, zero, or negative - 0.
+ *         Otherwise - -1.
+ */
+int handle_precision(va_list args, const char *modifier, char *index)
+{
+	int value = 0;
+
+	if (*modifier != '.')
+		return (-1);
+
+	modifier++;
+	(*index)++;
+
+	if ((*modifier <= '0' || *modifier > '9') &&
+	     *modifier != '*')
+	{
+		if (*modifier == '0')
+			(*index)++;
+		return (0);
+	}
+
+	while ((*modifier >= '0' && *modifier <= '9') ||
+	       (*modifier == '*'))
+	{
+		(*index)++;
+
+		if (*modifier == '*')
+		{
+			value = va_arg(args, int);
+			if (value <= 0)
+				return (0);
+			return (value);
+		}
+
+		value *= 10;
+		value += (*modifier - '0');
+		modifier++;
+	}
+
+	return (value);
+}
+
